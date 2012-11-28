d437f31c2c13f1c0d49fe5e2c15b68f15400fa95
mullerj <jmuller@teleinc.com>
2012-11-28 18:23:27 -0500

    Fixed menu item selection and manually call modal popup

--- SaluteASoldier/SaluteASoldier.Web/Scripts/utility.js
+++ SaluteASoldier/SaluteASoldier.Web/Scripts/utility.js
@@ -1,7 +1,7 @@
 ﻿function setActiveNavItem(itemTitle) {
     $(".nav li").each(function () {
         $(this).removeClass("active");
-        if ($(this).children("a")[0].text == itemTitle) {
+        if ($(this).children("a")[0] != undefined && $(this).children("a")[0].text == itemTitle) {
             $(this).addClass("active");
         }
     });
