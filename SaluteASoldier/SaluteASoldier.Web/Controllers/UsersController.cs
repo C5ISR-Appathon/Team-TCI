ab6c5cdab6983f8e78fad747705673aa7d2475f8
Bryan Billings <bcbillings@gmail.com>
2012-11-28 16:53:14 -0500

    Added UsersController for getting list of demo users

---
+++ SaluteASoldier/SaluteASoldier.Web/Controllers/UsersController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using SaluteASoldier.Web.Models;
+
+namespace SaluteASoldier.Web.Controllers
+{
+    public class UsersController : ApiController
+    {
+        private SaluteASoldierContext db = new SaluteASoldierContext();
+
+        // GET api/Users
+        public IEnumerable<User> GetUsers()
+        {
+            var users = db.Users.Include(u => u.UserType);
+            return users.AsEnumerable();
+        }
+
+        // GET api/Users/5
+        public User GetUser(int id)
+        {
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotFound));
+            }
+
+            return user;
+        }
+
+        // PUT api/Users/5
+        public HttpResponseMessage PutUser(int id, User user)
+        {
+            if (ModelState.IsValid && id == user.ID)
+            {
+                db.Entry(user).State = EntityState.Modified;
+
+                try
+                {
+                    db.SaveChanges();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound);
+                }
+
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+        }
+
+        // POST api/Users
+        public HttpResponseMessage PostUser(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Users.Add(user);
+                db.SaveChanges();
+
+                HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created, user);
+                response.Headers.Location = new Uri(Url.Link("DefaultApi", new { id = user.ID }));
+                return response;
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+        }
+
+        // DELETE api/Users/5
+        public HttpResponseMessage DeleteUser(int id)
+        {
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+
+            db.Users.Remove(user);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, user);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
