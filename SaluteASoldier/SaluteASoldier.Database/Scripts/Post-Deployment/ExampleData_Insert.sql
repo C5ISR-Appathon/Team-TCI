149d03af26f607fda7cfe3b549376753045cf1f8
Bryan Billings <bcbillings@gmail.com>
2012-11-28 19:35:29 -0500

    Added additional sample data

--- SaluteASoldier/SaluteASoldier.Database/Scripts/Post-Deployment/ExampleData_Insert.sql
+++ SaluteASoldier/SaluteASoldier.Database/Scripts/Post-Deployment/ExampleData_Insert.sql
@@ -16,9 +16,19 @@ SET IDENTITY_INSERT [dbo].[Message] ON
 INSERT [dbo].[Message] ([ID], [Text], [Screened], [Originator], [SubmissionDate], [Destination]) VALUES (1, N'Thank you so much for fighting for our country. You are the true reason for this holiday. You are the reason we are free to have it.', 1, 1, CAST(0x0000A11100000000 AS DateTime), 3)
 INSERT [dbo].[Message] ([ID], [Text], [Screened], [Originator], [SubmissionDate], [Destination]) VALUES (2, N'I just wanted to take a moment to salute you today! Thank you for your service, now & always.', 1, 1, CAST(0x0000A11100000000 AS DateTime), 2)
 INSERT [dbo].[Message] ([ID], [Text], [Screened], [Originator], [SubmissionDate], [Destination]) VALUES (5, N'You don''t know me and I don''t know you, but I''ve been there and from one soldier to another....stay strong.', 1, 4, CAST(0x0000A11100000000 AS DateTime), 2)
+INSERT [dbo].[Message] ([ID], [Text], [Screened], [Originator], [SubmissionDate], [Destination]) VALUES (6, N'You will always be remembered with respect and admiration.', 1, 1, CAST(0x0000A11100000000 AS DateTime), 3)
+INSERT [dbo].[Message] ([ID], [Text], [Screened], [Originator], [SubmissionDate], [Destination]) VALUES (7, N'Thank you for your service for our Freedom! May we never forget why we are free, thank you for making this possible!!!', 1, 1, CAST(0x0000A11100000000 AS DateTime), 2)
+INSERT [dbo].[Message] ([ID], [Text], [Screened], [Originator], [SubmissionDate], [Destination]) VALUES (8, N'Thank you for your service for our Freedom! May we never forget why we are free, thank you for making this possible!!!', 1, 1, CAST(0x0000A11100000000 AS DateTime), 3)
+INSERT [dbo].[Message] ([ID], [Text], [Screened], [Originator], [SubmissionDate], [Destination]) VALUES (9, N'I owe a never-ending debt of gratitude to you for your service and the sacrifices that you have made.', 1, 1, CAST(0x0000A11100000000 AS DateTime), 3)
+INSERT [dbo].[Message] ([ID], [Text], [Screened], [Originator], [SubmissionDate], [Destination]) VALUES (10, N'Thank you for your service for our Freedom! May we never forget why we are free, thank you for making this possible!!!', 1, 1, CAST(0x0000A11100000000 AS DateTime), 4)
 SET IDENTITY_INSERT [dbo].[Message] OFF
 SET IDENTITY_INSERT [dbo].[MessageAssignment] ON
 INSERT [dbo].[MessageAssignment] ([ID], [MessageID], [UserID], [Unread]) VALUES (1, 1, 4, 1)
 INSERT [dbo].[MessageAssignment] ([ID], [MessageID], [UserID], [Unread]) VALUES (2, 2, 2, 1)
 INSERT [dbo].[MessageAssignment] ([ID], [MessageID], [UserID], [Unread]) VALUES (5, 5, 2, 1)
+INSERT [dbo].[MessageAssignment] ([ID], [MessageID], [UserID], [Unread]) VALUES (6, 6, 4, 1)
+INSERT [dbo].[MessageAssignment] ([ID], [MessageID], [UserID], [Unread]) VALUES (7, 7, 2, 1)
+INSERT [dbo].[MessageAssignment] ([ID], [MessageID], [UserID], [Unread]) VALUES (8, 8, 4, 1)
+INSERT [dbo].[MessageAssignment] ([ID], [MessageID], [UserID], [Unread]) VALUES (9, 9, 4, 1)
+INSERT [dbo].[MessageAssignment] ([ID], [MessageID], [UserID], [Unread]) VALUES (10, 10, 6, 1)
 SET IDENTITY_INSERT [dbo].[MessageAssignment] OFF
